bb93f374cd08d86ce1da1ca9ec73ae6aa9f90089
Matthieu Robin <github@macolu.org>
2010-12-21 15:19:28 +0100

    translation of bonk page

--- applications/dashboard/views/error.master.php
+++ applications/dashboard/views/error.master.php
@@ -4,7 +4,7 @@ echo '<?xml version="1.0" encoding="utf-8"?>'; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-ca">
 <head>
-   <title>Bonk</title>
+   <title><?php echo T('Bonk') ?></title>
    <?php
    if ($CssPath !== FALSE)
       echo '<link rel="stylesheet" type="text/css" href="',Asset($CssPath),'" />';
@@ -13,8 +13,8 @@ echo '<?xml version="1.0" encoding="utf-8"?>'; ?>
 <body>
    <div id="Content">
       <div class="SplashInfo">
-         <h1>Bonk</h1>
-         <p>Something funky happened. Please bear with us while we iron out the kinks.</p>
+         <h1><?php echo T('Bonk') ?></h1>
+         <p><?php echo T('Something funky happened. Please bear with us while we iron out the kinks.') ?></p>
       </div>
    </div>
 </body>
